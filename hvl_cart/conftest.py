ab480c4ba94bcf0e2247fd7bb77f32cc3bcb171b
Erdem Ozgen <m.erdemozgen@gmail.com>
2023-03-30 16:16:08 +0300

    Havelsan Django base template init

---
+++ hvl_cart/conftest.py
@@ -0,0 +1,14 @@
+import pytest
+
+from hvl_cart.users.models import User
+from hvl_cart.users.tests.factories import UserFactory
+
+
+@pytest.fixture(autouse=True)
+def media_storage(settings, tmpdir):
+    settings.MEDIA_ROOT = tmpdir.strpath
+
+
+@pytest.fixture
+def user(db) -> User:
+    return UserFactory()
